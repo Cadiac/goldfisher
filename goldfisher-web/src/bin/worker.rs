f802064b65b704f6d7376a5ab66c331f093a59b7
Jaakko Husso <jaakko.t.husso@gmail.com>
2022-08-17 19:59:41 +0300

    Test if WeeAlloc has any effect on worker performance on repeated runs

--- goldfisher-web/src/bin/worker.rs
+++ goldfisher-web/src/bin/worker.rs
@@ -2,8 +2,8 @@ use goldfisher_web::Goldfish;
 
 use gloo_worker::Registrable;
 
-#[global_allocator]
-static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+// #[global_allocator]
+// static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
 
 fn main() {
